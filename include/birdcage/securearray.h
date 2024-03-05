d212fd5c7d738f66d84eba3c71ab85cc45f0a4e3
Warren MacEvoy <wmacevoy@gmail.com>
2024-03-05 12:22:07 -0700

    includes

---
+++ include/birdcage/securearray.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "securedata.h"
+#include <array>
+
+namespace birdcage {
+template <typename T,size_t Size>
+  void SecureArrayReset(std::array < T , Size > &data) { data.fill(T()); }
+
+// SecureArray is SecureData with a reset that fills with default T values
+template <typename T,size_t Size>
+  using SecureArray = SecureData < std::array < T , Size >, &SecureArrayReset<T,Size> >;
+} // namespace birdcage
---
+++ include/birdcage/securedata.h
@@ -0,0 +1,50 @@
+#include <functional>
+
+#include "canary.h"
+
+namespace birdcage {
+void SecureDataLock(void *base, size_t len);
+void SecureDataUnlock(void *base, size_t len);   
+
+// default template reset for data type (reset to default)
+template <typename Type>
+void SecureDataReset(Type &data) {
+  data = Type();
+}
+
+
+//
+// Wrap a Type between Canary values and that is locked out of paging.
+// They can be checked at any time, but are automatically reset & checked on destruction.
+//
+template<typename _Type, auto _Reset = &SecureDataReset<_Type> >
+class SecureData {
+ public: using Type = _Type;
+ public: const Canary before;
+ public: _Type data;
+ public: const Canary after;
+
+ public: void check() const {
+    before.check(); 
+    after.check(); 
+  }
+
+ public: void reset() {
+    //    _Reset reset;
+    _Reset(data);
+    before.check();
+    after.check();
+  }
+  
+ public: SecureData() {
+    SecureDataLock(&data,sizeof(_Type));
+    reset();
+  }
+  
+ public: ~SecureData() { 
+    reset();
+    SecureDataUnlock(&data,sizeof(_Type));
+  }
+};
+
+} // namespace birdcage
